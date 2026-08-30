6954e7c9a1b2028023d55b49176d6ac0126a8def
Dylan Umsted <dylan@umsted.org>
2026-08-29 22:23:54 -0700

    Add closure initializer and update copyright header

--- Sources/SwiftAxiom/RDF/Primitives/Statement.swift
+++ Sources/SwiftAxiom/RDF/Primitives/Statement.swift
@@ -1,7 +1,9 @@
 //
 //  Statement.swift
-//  SwiftAxiom RDF Primitives
-//  © 2026 Dylan Umsted <dylan@umsted.org>
+//  SwiftAxiom / RDF / Primitives
+//
+//  Created on 2026-08-28 by Dylan Umsted.
+//  © 2026 Dylan Umsted <dylan@umsted.org>.
 //
 
 /// Represents a single triple (subject–predicate–object) or quad (subject–predicate–object–graph) statement.
@@ -20,10 +22,24 @@ public struct Statement: Sendable {
     public let graph: IRI?
 
     /// Creates a subject–predicate–object statement with an optional graph context.
-    public init(_ subject: RDFSubject, _ predicate: IRI, _ object: RDFObject, _ graph: IRI? = nil) {
+    public init(_ subject: RDFSubject, _ predicate: RDFPredicate, _ object: RDFObject, _ graph: IRI? = nil) {
         self.subject = subject
         self.predicate = predicate
         self.object = object
         self.graph = graph
+    }
+
+    /// Creates a subject–predicate–object statement with an optional graph context using closures for result building
+    /// and deferred evaluation.
+    public init(
+        subject: () -> RDFSubject,
+        predicate: () -> RDFPredicate,
+        object: () -> RDFObject,
+        graph: () -> IRI? = { nil }
+    ) {
+        self.subject = subject()
+        self.predicate = predicate()
+        self.object = object()
+        self.graph = graph()
     }
 }
