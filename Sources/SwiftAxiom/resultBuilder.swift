4d8885c557c5dc0fface1c6bb147cbc87ad77e5b
Dylan Umsted <dylan@umsted.org>
2026-08-29 22:24:41 -0700

    Expand on resultBuilder example

--- Sources/SwiftAxiom/resultBuilder.swift
+++ Sources/SwiftAxiom/resultBuilder.swift
@@ -53,4 +53,7 @@ public struct Graph: Sendable {
 let someGraph: Graph = Graph {
     Base { "https://example.com/" }
     Prefix("ex") { "https://example.com/" }
+    Prefix("foaf") { "https://xmlns.com/foaf/0.1/" }
+    Serialization.ttl(.v1_2)
+    
 }
